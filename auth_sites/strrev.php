06aece8a6ea5635aa8b6f55d888611e87d907178
Dalgoaaa <drekhlitskii@gmail.com>
2021-04-03 12:01:35 +0300

    delete old files

--- auth_sites/strrev.php
+++
@@ -1,8 +0,0 @@
-<?php
-function mb_strrev($text)
-{
-    return join('', array_reverse(preg_split('//u', $text, -1, PREG_SPLIT_NO_EMPTY)));
-}
-
-echo mb_strrev((htmlspecialchars($_POST["text"])));
-?>
--- connectdb.php
+++
@@ -1,2 +0,0 @@
-<?php
-$connection = mysqli_connect("loca_mysql", "root", "dbggj_bggei-bgch", "auth" );
